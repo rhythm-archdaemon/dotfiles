d607895da1654f8998367ea51044a953142b563a
rhythm-archdaemon <rhythmbhattarai@proton.me>
2026-07-09 22:26:04 +0545

    keybinds changed to follow vim motions and use rofi as application manager

--- .config/niri/start-wallpaper.sh
+++ .config/niri/start-wallpaper.sh
@@ -1,11 +1,11 @@
 # kills previous instance of wallpaper engine on execution
 # pkill awww-daemon
 
-# opens: wallpaper engine ----- configiure the code below ----------------------------    
+# opens: wallpaper engine ----- configiure the code below ----------------------------
 awww-daemon &
 
 sleep 0.5
 
-awww img "/home/rhythm/Pictures/Wallpapers/walls/solarized/a_red_sun_over_mountains.jpg" \
+awww img "/home/rhythm/Pictures/Wallpapers/walls/unsorted/a_car_on_a_wet_road.jpg" \
   --transition-type wave \
   --transition-duration 1
